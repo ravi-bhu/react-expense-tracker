99cd5ee1a8b1f25c9bc94d4d25298fe7884555f9
Ravi Bhushan <ravibhushan122@gmail.com>
2021-08-28 23:19:43 +0530

    removed useless header

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,6 @@ import './Expenses.css';
 const Expenses = (props) => {
   return (
     <Card className="expenses">
-      <h2>Let's get started!</h2>
       <ExpenseItem expense={props.expenses[0]} />
       <ExpenseItem expense={props.expenses[1]} />
       <ExpenseItem expense={props.expenses[2]} />
